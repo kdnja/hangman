ab59a15c0d55f184413d2f99a21e2b2cf8b78767
Caden <me@kdn.pw>
2022-06-01 12:30:37 -0700

    Added code to read file data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate rand;
+use rand::Rng;
+
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    
+    let selected_word = select_word();
+
+    println!("The selected word was {}", selected_word)
+}
+
+fn select_word() -> String {
+    /* Open file */
+    let mut file = File::open("src/words.txt").expect("Could not open file!");
+
+    /* Load file contents */
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .expect("An error has occurred while reading the file!");
+
+    /* Get individual words */
+    let available_words: Vec<&str> = file_contents.trim().split(',').collect();
+
+    /* Generate random index */
+    let random_index = rand::thread_rng().gen_range(0..available_words.len());
+
+    return String::from(available_words[random_index]);
 }
